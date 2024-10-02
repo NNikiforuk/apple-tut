4d31272fd36a5bdf19560bd92b34fffcb2dee618
Natalia Nikiforuk <natnikiforuk@gmail.com>
2024-10-02 19:48:44 +0200

    Initial Commit

---
+++ apple tut/apple_tutApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_tutApp.swift
+//  apple tut
+//
+//  Created by Natalia Nikiforuk on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct apple_tutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
